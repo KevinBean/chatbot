eb6e6309224348f3dab35def675b5cac486bb54f
KevinBean <bianbin@gmail.com>
2016-08-20 11:26:37 +0800

    微调

--- attract.py
+++ attract.py
@@ -71,7 +71,7 @@ def findinFile(filename, dictionary, sample): #使用tfidf
     (x, y) = sims[0]
     print lines[x]
 
-def findinFiles(filename, sample): #使用tfidf
+def findinFiles(filename, samples): #使用tfidf
     dictionary = filetoDict(filename, dictpath)
     for file in filename:
         lines = cutlines(file)
@@ -87,29 +87,33 @@ def findinFiles(filename, sample): #使用tfidf
         # for doc in corpus_tfidf:
         #    print doc
 
-        # 将查询文档转到tfidf
-        sample_tfidf = tfidf[dictionary.doc2bow(jieba.lcut(sample, cut_all=False))]
-
         # 查询相似度
         index = similarities.MatrixSimilarity(corpus_tfidf)
+
+        # 将查询文档转到tfidf
+        sample_tfidf = tfidf[dictionary.doc2bow(jieba.lcut(sample, cut_all=False))]
         sims = index[sample_tfidf]
-        print list(enumerate(sims))
+        # print list(enumerate(sims))
 
         # 排序输出,查询文档和0，7，2，3号文档相似性较高
         sims = sorted(enumerate(sims), key=lambda item: item[1], reverse=True)
-        print sims
-        print sims[0]
+        # print sims
+        # print sims[0]
         (x, y) = sims[0]
-        print file
-        print lines[x]
+        # print file
+        return lines[x]
 
 
 
 
-filename = [u'doc/SM021C-A11-01.txt', u'doc/SL351C-A11-01.txt'] # u'doc/管廊缆线敷设技术条件.docx'
-dictpath = u'dict/SM021C-A11-01.txt'
+filename = [u'doc/SL351C-A11-01.txt'] # u'doc/管廊缆线敷设技术条件.docx'
+dictpath = u'dict/dict.txt'
+gongchengming = u'南营110kV送电工程'
+gongchengbianhao = u'SM221C-A11-01'
 jiedi = u'本工程单回电缆采用两端直接接地的接地方式，即在电缆线路两端电缆终端处，电缆的金属屏蔽层及铠装层经接地引线分别引出，在灰峪站内与站内接地网做可靠连接、在终端塔侧与铁塔接地极引线做可靠连接。'
 gaikuang = u'南营110kV变电站拟建于丰台区长辛店北部地区，长辛店北部地区位于丰台永定河绿色生态发展带，目前处于全面发展的阶段，规划重点项目包括辛庄一级开发项目、201所改造项目、解放军通信团项目、丰台科技园西区II期等。其中，丰台科技园西区II期项目，总建筑面积150万平方米，主要为技术创新基地、科技成果孵化基地、高新技术产业化基地、教育科研、办公等用地，负荷预测约60MW；201所改造项目未来负荷将达到25MW；辛庄土地一级开发项目总建筑面积约129万平方米，主要为居住及配套共建用地，负荷预测约为18MW；解放军通信团项目总建筑面积约10万平方米，主要为军事类用地，负荷预测约15MW；第九届园艺博览会项目，占地267公顷，总建筑面积16万平方米，主要为旅游、商业、公建用地，负荷预测约20MW。未来该区域负荷将达到138MW。详情见下表2-1所示。'
+samples = [gongchengbianhao, gongchengming, jiedi, gaikuang]
 dictionary = filetoDict(filename, dictpath)
-findinFile(filename[0], dictionary, jiedi)
-findinFiles(filename, gaikuang)
+# findinFile(filename[0], dictionary, jiedi)
+for sample in samples:
+    print findinFiles(filename, sample) .decode('utf-8')
