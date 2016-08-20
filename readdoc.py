618db305bf636d6bbd3d1086f9f311ac0be54280
KevinBean <bianbin@gmail.com>
2016-08-20 11:27:31 +0800

    增加读取doc文件功能

--- readdoc.py
+++ readdoc.py
@@ -3,9 +3,11 @@
 from docx import Document #使用python-docx包
 import jieba # 利用jieba进行中文分词
 import nltk  # 利用nltk进行其他处理
+from win32com import client as wc
+import os
 
 
-def doc2txt(filename):
+def docx2txt(filename):
     # 从word（docx格式）中提取text，保存为txt
     document = Document(filename)
     docText = '\n\n'.join([
@@ -20,11 +22,28 @@ def doc2txt(filename):
     output_file.write(docText)
     output_file.close()
 
+def doc2txt(filename):
+    word = wc.Dispatch('Word.Application')
+    filename = os.path.normpath(filename)
+    print filename
+    doc = word.Documents.Open(filename) # 所有Paragraph即段落对象，都是通过Paragraph.Range.Text来访问它的文字的
+    docText = '\n\n'.join([
+                              paragraph.Range.Text.encode('utf-8') for paragraph in doc.paragraphs
+                              ])
+    word.Quit()
+    newfilename = filename.replace(u'.doc', u'.txt')
+    print newfilename
+    output_file = open(newfilename, 'w')
+    output_file.write(docText)
+    output_file.close()
 
 if __name__ == '__main__':
-    filename = u'doc/SL351C-A11-01.docx' # u'doc/管廊缆线敷设技术条件.docx'
-    # doc2txt(filename)
-    newfilename = filename.replace(u'docx', u'txt')
+    filename = ur'D:\Personal\我的文档\GitHub\chatbot\doc\X9384K-AB-01.doc' # u'doc/管廊缆线敷设技术条件.docx'
+    filename = os.path.normpath(filename)
+    print filename
+    # filename = r'd:/test1.doc'
+    doc2txt(filename)
+    newfilename = filename.replace(u'.doc', u'.txt')
     read_file = open(newfilename,'r')
     text = read_file.read()
     text = text.replace('  ',' ')
