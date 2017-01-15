a3187d9e5db0c837a6460629b6def39f70cfdc83
KevinBean <bianbin@bianbindeiMac.local>
2017-01-15 20:22:28 +0800

    scrapemark scrapely 测试失败

--- doc/zlxx/patternattract.py
+++ doc/zlxx/patternattract.py
@@ -1,4 +1,5 @@
 # -*- coding=utf-8 -*-
+''' scrapemark无法从pip获得，本地安装成功，但是找不到地址
 from scrapemark import scrape
 
 docdir = sys.path[0]
@@ -19,5 +20,37 @@ for root, dirs, files in doclist:
                """,
                url='http://someurl.com/')
 
+'''
+import chardet
+from scrapely import Scraper
+from scrapely.htmlpage import HtmlPage
+s = Scraper()
+url1 = '/Users/bianbin/PycharmProjects/chatbot/doc/zlxx/01质量信息反馈单－刘毅梅.txt'
+input_file = open(url1, 'r')
+txt = input_file.read()
+# print txt, chardet.detect(txt)
+# body = txt.decode('gb2312')
+# print body, chardet.detect(body), type(body)
+input_file.close()
+page1 = HtmlPage(body=txt.decode('utf-8'), encoding='utf-8')
+print txt, page1.body, 'body', page1.url, page1.encoding
+data = {
+        'description': u'在陈官屯220kV变电站工程通信施工图设计中，秦皇岛供电公司提出站内除光缆外的所有线缆均要求选用阻燃铠装电缆，此要求是冀北公司统一要求。'}
+s.train_from_htmlpage(page1, data)
 
+url2 = '/Users/bianbin/PycharmProjects/chatbot/doc/zlxx/01质量信息反馈单－李雪男.txt'
+input_file = open(url2, 'r')
+txt = input_file.read()
+input_file.close()
+page2 = HtmlPage(body=txt.decode('utf-8'), encoding='utf-8')
+data = s.scrape_page(page2)
+print data, 'end'
 
+'''
+url1 = 'http://pypi.python.org/pypi/w3lib/1.1'
+data = {'name': 'w3lib 1.1', 'author': 'Scrapy project', 'description': 'Library of web-related functions'}
+s.train(url1, data)
+
+url2 = 'http://pypi.python.org/pypi/Django/1.3'
+data = s.scrape(url2)
+'''
